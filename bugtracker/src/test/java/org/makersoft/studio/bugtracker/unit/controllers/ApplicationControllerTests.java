b36d1e54224e95916802734ce28d98446d538a6b
fengkuok <fengkuok@gmail.com>
2013-05-06 10:54:53 +0800

    add unit test resources

---
+++ bugtracker/src/test/java/org/makersoft/studio/bugtracker/unit/controllers/ApplicationControllerTests.java
@@ -0,0 +1,19 @@
+/*
+ * @(#)ApplicationControllerTests.java 2013-5-6 下午23:33:33
+ *
+ * Copyright (c) 2011-2013 Makersoft.org all rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ */
+package org.makersoft.studio.bugtracker.unit.controllers;
+
+/**
+ * Class description goes here.
+ * 
+ * @version 2013-5-6 上午10:54:14
+ * @author Feng Kuok
+ */
+public class ApplicationControllerTests {
+
+}
